b8a78a507361299127195c5c7b67a00b2c91beb1
Dar'yuwka <daryaryab@gmail.com>
2016-09-05 11:31:21 +0300

    delete all

--- course_project/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    		$(".arrow-click").click(function(){
-        	$(".slide").show();
-    });
-    		$(".close-icon").click(function(){
-        	$(".slide").hide();
-    });
-});
