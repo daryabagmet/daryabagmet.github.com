851587bb7c76be2c1689c410b41a57baeb6ae53d
Dar'yuwka <daryaryab@gmail.com>
2016-09-13 18:20:26 +0300

    changes 13/09

--- bakery_project/js/main.js
+++ bakery_project/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   $('.bxslider').bxSlider();
 });
+$(document).ready(function(){
+	$(".nav-mobile").click(function(){
+		$(".main-menu").slideToggle("slow");
+	});
+});
