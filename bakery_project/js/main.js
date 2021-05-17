78ac1a8895569c59c45c62ba24e85e761b08ada9
Darya <pirozho@i.ua>
2021-05-17 21:56:55 +0300

    Delete bakery_project directory

--- bakery_project/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.bxslider').bxSlider();
-});
-$(document).ready(function(){
-	$(".nav-mobile").click(function(){
-		$(".main-menu").slideToggle("slow");
-	});
-});
